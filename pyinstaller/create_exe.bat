25192693baeb4e5952a450f8f838578e077099ae
René <snooz@posteo.de>
2025-11-21 20:13:15 +0100

    fixed pyinstaller icon

--- pyinstaller/create_exe.bat
+++ pyinstaller/create_exe.bat
@@ -1,3 +1,3 @@
 pushd %~dp0
-pyinstaller -i ../imbusTB.ico --onefile --name TestBenchCliReporter --clean -y --distpath . run.py
+pyinstaller -i imbusTB.ico --onefile --name TestBenchCliReporter --clean -y --distpath . run.py
 popd
--- pyinstaller/create_mac_app.sh
+++ pyinstaller/create_mac_app.sh
@@ -2,6 +2,6 @@
 
 pushd "$(dirname "$0")" > /dev/null
 
-pyinstaller -i ../imbusTB.ico --onefile --name TestBenchCliReporter --clean -y --distpath . run.py
+pyinstaller -i imbusTB.ico --onefile --name TestBenchCliReporter --clean -y --distpath . run.py
 
 popd > /dev/null
