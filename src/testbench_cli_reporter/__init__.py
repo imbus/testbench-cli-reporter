660a50cb5fccb57a232ac0b05764431a7990fbd5
René <snooz@posteo.de>
2026-03-25 00:50:14 +0100

    Release 3.0.0
    Signed-off-by: René <snooz@posteo.de>

--- src/testbench_cli_reporter/__init__.py
+++ src/testbench_cli_reporter/__init__.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__version__ = "3.0.0rc4"
+__version__ = "3.0.0"
 
 from .config_model import (
     BaseAction,
